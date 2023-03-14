71aa5aea16036f45f085d3f5f24957995b485a3e
Patryk Gruchot <patryk.gruchot@outlook.com>
2023-03-14 18:02:58 +0100

    basic styling and initial shop categories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-const App = () => {
+import "./categories.styles.scss";
 
-    return <div>
-      Konnichiwa
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womans",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+  return (
+    <div className="catergories-container">
+      {categories.map(({ id, title }) => (
+        <div className="category-container" key={id}>
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
-}
+  );
+};
 
 export default App;
