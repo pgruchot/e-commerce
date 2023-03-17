b57566936a4d9a844a21cf36dcd13d1c6963feb8
Patryk Gruchot <patryk.gruchot@outlook.com>
2023-03-17 15:37:46 +0100

    new logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Link } from "react-router-dom";
-import { ReactComponent as JpnLogo } from "../../assets/jpn.svg";
+import JpnLogo from "../../assets/jpn-logo.component";
 import "./navigation.styles.scss";
 const Navigation = () => {
   return (
