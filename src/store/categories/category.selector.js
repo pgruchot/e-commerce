34d1c6d831870b19e582763a4cbc5057c7178dff
Patryk Gruchot <patryk.gruchot@outlook.com>
2023-03-31 00:12:02 +0200

    moved user and category context to Redux

---
+++ src/store/categories/category.selector.js
@@ -0,0 +1,8 @@
+import { createSelector } from "reselect";
+
+export const selectCategoriesMap = (state) =>
+  state.categories.categories.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
